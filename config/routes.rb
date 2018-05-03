c44dbf79d2dcc7b71be381ea3de04da97b5fbb83
Shumai <shumai9@github.com>
2018-05-03 23:35:53 +0100

    set up join/leave room links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  get 'chatroom_users/create'
+  get 'chatroom_users/destroy'
+  get 'chatroom_users_controller/create'
+  get 'chatroom_users_controller/destroy'
   devise_for :users
-  resources :chatrooms
+
+  resources :chatrooms do
+    resource :chatroom_users
+  end
+ #we use this to acces the join table and grab users by id
+    #by changing the nested resources from purlar to singular..it saves the app from
+    # crushing should user try to leave room that they didn't join.
+ 
+
   root to: 'chatrooms#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
